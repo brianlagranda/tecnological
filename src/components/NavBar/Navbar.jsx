7777ccff0715106f433a3bbf6f3b26533ad43cd6
Brian <brianlagranda@gmail.com>
2022-12-27 23:29:02 -0300

    PreEntrega1+Lagranda

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import CartWidgetIcon from '../CartWidget/CartWidget';
+
+export default function NavBar() {
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container fluid>
+                    <Col>
+                        <Navbar.Brand href="#home">Tecnological</Navbar.Brand>
+                    </Col>
+                    <Col>
+                        <Nav className="justify-content-end">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#products">Products</Nav.Link>
+                            <Nav.Link href="#help">Help</Nav.Link>
+                        </Nav>
+                    </Col>
+                    <Col md="auto">
+                        <CartWidgetIcon />
+                    </Col>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
