618bab78501329df688b0d22233d56e111083baf
Brian <brianlagranda@gmail.com>
2022-12-27 23:29:47 -0300

    PreEntrega1+Lagranda

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Stack from 'react-bootstrap/Stack';
+import styles from './ItemListContainer.module.css';
+
+export default function ItemListContainer() {
+    return (
+        <Stack gap={2} className="col-md-3 mx-auto">
+            <div className={styles.greeting}>Bienvenid@s! Encontrá los componentes de pc más buscados.</div>
+        </Stack>
+    );
+}
