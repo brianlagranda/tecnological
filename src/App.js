f2d620faab0c799b29178a3c73215088bde614f9
Brian <brianlagranda@gmail.com>
2023-01-04 20:20:43 -0300

    Cleaning files without use

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
+
 function App() {
   return (
     <>
---
+++ test.js
@@ -0,0 +1,6 @@
+let sum = ["abc", "def", "jk"]
+let sum1 = 20
+
+let result = 0
+
+sum.fill
