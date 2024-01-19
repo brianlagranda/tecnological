1158474b59eb094b6d13ab1b9b432f869f47e0a5
Briandvlpr <brianlagranda@gmail.com>
2024-01-19 12:33:41 -0300

    feat: Controls and images adjustments.

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -10,83 +10,89 @@ import './itemdetail.css';
 import { ButtonChild } from '../button/Button';
 
 function ItemDetailContainer() {
-  const [isReady, setReady] = useState(false);
-  const [fullCart, setCart] = useState(false);
-  const [item, setItem] = useState([]);
-  const { findItem } = useContext(cartContext);
+    const [isReady, setReady] = useState(false);
+    const [fullCart, setCart] = useState(false);
+    const [item, setItem] = useState([]);
+    const { findItem } = useContext(cartContext);
 
-  const params = useParams();
-  const id = params.id;
+    const params = useParams();
+    const id = params.id;
 
-  const itemFind = findItem(item.id);
+    const itemFind = findItem(item.id);
 
-  let realStock;
+    let realStock;
 
-  if (itemFind) {
-    realStock = item.stock - itemFind.cant;
-  } else {
-    realStock = item.stock;
-  }
-
-  async function getItemAsync() {
-    try {
-      let res = await getSingleItem(id);
-      setItem(res);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setReady(false);
-    }
-  }
-
-  useEffect(() => {
-    getItemAsync();
-  }, []);
-
-  const { addItemToCart } = useContext(cartContext);
-
-  function addToCart(quantity) {
-    item.cant = quantity;
-    addItemToCart(item);
-    if (addItemToCart(item)) {
-      toast.success('The product has been added to your cart');
-      setCart(true);
+    if (itemFind) {
+        realStock = item.stock - itemFind.cant;
     } else {
-      toast.error('The product is already in your cart');
+        realStock = item.stock;
     }
-  }
 
-  if (isReady) return <Spinner />;
+    async function getItemAsync() {
+        try {
+            let res = await getSingleItem(id);
+            setItem(res);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setReady(false);
+        }
+    }
 
-  return (
-    <>
-      <div className='card-detail_main'>
-        <div className='card-detail_img'>
-          <img src={item.imgurl} alt='img' />
-        </div>
+    useEffect(() => {
+        getItemAsync();
+    }, []);
 
-        <div className='card-detail_detail'>
-          <h1 className='cardDetail_title'>{item.title}</h1>
-          <h2 className='cardDetail_price'>$ {item.price}</h2>
-          <p className='cardInfo'>{item.detail}</p>
-          {fullCart === false ? (
-            <div className='cardDetail_buttons'>
-              <ItemCount addToCart={addToCart} start={0} stock={realStock} />
+    const { addItemToCart } = useContext(cartContext);
+
+    function addToCart(quantity) {
+        item.cant = quantity;
+        addItemToCart(item);
+        if (addItemToCart(item)) {
+            toast.success('The product has been added to your cart');
+            setCart(true);
+        } else {
+            toast.error('The product is already in your cart');
+        }
+    }
+
+    if (isReady) return <Spinner />;
+
+    return (
+        <>
+            <div className='card-detail_main'>
+                <h1 className='cardDetail_title'>{item.model}</h1>
+                <div className='card-detail_img'>
+                    <img src={item.imgurl} alt='img' />
+                </div>
+
+                <div className='card-detail_detail'>
+                    <h2 className='cardDetail_price'>$ {item.price}</h2>
+                    <p className='cardInfo'>{item.detail}</p>
+                    {fullCart === false ? (
+                        <div className='cardDetail_buttons'>
+                            <ItemCount
+                                addToCart={addToCart}
+                                start={0}
+                                stock={realStock}
+                            />
+                        </div>
+                    ) : (
+                        <>
+                            <Link to='/cart'>
+                                <ButtonChild>Ir al carrito</ButtonChild>
+                            </Link>
+
+                            <p className='cartInfo'>
+                                El producto ha sido añadido al carrito
+                            </p>
+                        </>
+                    )}
+                </div>
             </div>
-          ) : (
-            <>
-              <Link to='/cart'>
-                <ButtonChild>Ir al carrito</ButtonChild>
-              </Link>
-
-              <p className='cartInfo'>El producto ha sido añadido al carrito</p>
-            </>
-          )}
-        </div>
-      </div>
-      <ToastContainer />
-    </>
-  );
+            <ToastContainer />
+        </>
+    );
 }
 
 export default ItemDetailContainer;
