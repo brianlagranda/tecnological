7f539de7f293fc71fee3af81c5da29d3060c0143
Briandvlpr <brianlagranda@gmail.com>
2024-01-19 12:32:55 -0300

    feat: Fixed bug when on max quantity it was stuck

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -3,46 +3,46 @@ import { useState } from 'react';
 import './itemcount.css';
 
 const ItemCount = ({ addToCart, stock }) => {
-  const [quantity, setQuantity] = useState(0);
+    const [quantity, setQuantity] = useState(0);
 
-  const addItem = num => {
-    if (stock > quantity) setQuantity(quantity + num);
-  };
+    const addItem = (num) => {
+        if (stock >= quantity) setQuantity(quantity + num);
+    };
 
-  const handleClick = () => {
-    quantity > 0
-      ? addToCart(quantity)
-      : alert('La cantidad debe ser mayor a 0');
-  };
+    const handleClick = () => {
+        quantity > 0
+            ? addToCart(quantity)
+            : alert('La cantidad debe ser mayor a 0');
+    };
 
-  return (
-    <div className='itemcount_container'>
-      <small>Agregá la cantidad deseada al carrito</small>
-      <div className='itemcount_control'>
-        <button
-          className='btn'
-          onClick={() => addItem(-1)}
-          disabled={quantity === 0}
-        >
-          -
-        </button>
-        <span className='quantityInput'>{quantity}</span>
-        <button
-          className='btn'
-          onClick={() => addItem(1)}
-          disabled={quantity === stock}
-        >
-          +
-        </button>
-      </div>
+    return (
+        <div className='itemcount_container'>
+            <small>Agregá la cantidad deseada al carrito</small>
+            <div className='itemcount_control'>
+                <button
+                    className='btn'
+                    onClick={() => addItem(-1)}
+                    disabled={quantity === 0}
+                >
+                    -
+                </button>
+                <span className='quantityInput'>{quantity}</span>
+                <button
+                    className='btn'
+                    onClick={() => addItem(1)}
+                    disabled={quantity === stock}
+                >
+                    +
+                </button>
+            </div>
 
-      <div className='itemcount_btns'>
-        <button className='btn' onClick={handleClick}>
-          Add to cart
-        </button>
-      </div>
-    </div>
-  );
+            <div className='itemcount_btns'>
+                <button className='btn' onClick={handleClick}>
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ItemCount;
